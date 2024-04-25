2fddfd59616cfc5ae1ea9d7fda1b909d3735351d
jasonokingo <jason.okingo@student.moringaschool.com>
2024-04-25 11:01:45 +0300

    Feat:Updated

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { image } from "../data/data";
+
+function About() {
+  return <div id="about">
+          <h2>About Me</h2>
+          <p>AmbassadorOfTheGhetto</p>
+          <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        </div>;
+}
+
+export default About;
